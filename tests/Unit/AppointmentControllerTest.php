ba401cb722fa592030de00da9c369ba7c1249625
Marcos Vinicius <marcosvoliveira19964@gmail.com>
2025-03-18 23:46:30 -0300

    atualização no teste registros

--- tests/Unit/AppointmentControllerTest.php
+++ tests/Unit/AppointmentControllerTest.php
@@ -50,6 +50,7 @@ class AppointmentControllerTest extends TestCase
             'replace' => false,
             'showMore' => false,
             'photo' => UploadedFile::fake()->image('photo.jpg'),
+            'accommodation_mode' => 'pernoite',
         ];
 
         $response = $this->postJson('/api/appointments', $data);
@@ -82,10 +83,11 @@ class AppointmentControllerTest extends TestCase
             'isHidden' => false,
             'replace' => false,
             'showMore' => false,
+            'accommodation_mode' => 'pernoite',
         ];
 
         $response = $this->postJson('/api/appointments', $data);
         
         $response->assertStatus(409);
-    }
+    } 
 }
